f5e929eda6b946f93f29e5aeba70ebc532744d76
WindNguyendev <134860575+WindNguyendev@users.noreply.github.com>
2024-11-15 21:11:40 +0700

    update 15_11_2024

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.laptopshop;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
     @GetMapping("/")
