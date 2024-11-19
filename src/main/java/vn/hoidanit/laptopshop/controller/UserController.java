f67ab7c7d157fac1bb4afa6918def435070fe907
WindNguyendev <134860575+WindNguyendev@users.noreply.github.com>
2024-11-20 04:21:55 +0700

    update_20_11_2024

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,10 +1,14 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -16,28 +20,24 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
 
         String test = this.userService.handleHello();
+        model.addAttribute("wind", test);
 
         return "hello";
     }
 
+    @RequestMapping("/admin/user")
+    public String getCreateUserAdmin(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String CreateUserPage(Model model, @ModelAttribute("newUser") User wind) {
+        System.out.println("run here" + wind);
+        return "hello";
+    }
+
 }
-
-// @RestController
-// public class UserController {
-
-// // DI: dependency injection
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-
-// }
